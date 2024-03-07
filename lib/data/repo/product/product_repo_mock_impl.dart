46d39e080c73494a535b8bb97eb1933aba738a4e
rajan-metaliya <rajanmetaliya77@gmail.com>
2024-03-07 11:53:56 +0530

    Update package and dependency

--- lib/data/repo/product/product_repo_mock_impl.dart
+++ lib/data/repo/product/product_repo_mock_impl.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import '../../models/product_model.dart';
 import 'product_repo.dart';
 
--- lib/feature/products/widgets/product_card.dart
+++ lib/feature/products/widgets/product_card.dart
@@ -8,10 +8,10 @@ class ProductCard extends StatelessWidget {
   final Function() addToCart;
 
   const ProductCard({
-    Key? key,
+    super.key,
     required this.product,
     required this.addToCart,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
