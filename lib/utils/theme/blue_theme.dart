7433a7274fcff947405a510829c2db8f32b8cb32
rajan-metaliya <rajanmetaliya77@gmail.com>
2024-03-11 17:31:33 +0530

    app theme

--- lib/utils/theme/blue_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import '../constants/assets_path.dart';
-
-Future<ColorScheme> getBlueColorScheme() async {
-  return await ColorScheme.fromImageProvider(
-    provider: const AssetImage(AssetsPath.blueThemeImage),
-  );
-}
